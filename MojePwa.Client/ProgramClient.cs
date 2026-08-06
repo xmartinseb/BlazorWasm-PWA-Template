fe969f4a04d3f3572c6bd55399b8f7b89515261a
Martin Sebera <xmartin.sebera@gmail.com>
2026-08-06 19:53:34 +0200

    LocalStorage

--- MojePwa.Client/ProgramClient.cs
+++ MojePwa.Client/ProgramClient.cs
@@ -2,6 +2,7 @@ global using CT = System.Threading.CancellationToken;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MojePwa.Client;
+using MojePwa.Client.Services.Browser;
 using MojePwa.Client.Services.DataServices;
 using Radzen;
 
@@ -13,5 +14,6 @@ builder.Services.AddRadzenComponents();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IWeatherService, WeatherService>();
+builder.Services.AddScoped<LocalStorage>();
 
 await builder.Build().RunAsync();
---
+++ MojePwa.Client/Services/Browser/LocalStorage.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.JSInterop;
+using System.Text.Json;
+
+namespace MojePwa.Client.Services.Browser;
+
+public sealed class LocalStorage(IJSRuntime js)
+{
+    public ValueTask SetAsync(string key, string value)
+        => js.InvokeVoidAsync("localStorage.setItem", key, value);
+
+    public ValueTask SetAsync<T>(string key, T value)
+        => js.InvokeVoidAsync("localStorage.setItem", key, JsonSerializer.Serialize(value));
+
+    public ValueTask<string?> GetAsync(string key)
+        => js.InvokeAsync<string?>("localStorage.getItem", key);
+
+    public async Task<T?> GetAsync<T>(string key)
+    {
+        var json = await js.InvokeAsync<string?>("localStorage.getItem", key);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return default;
+        }
+
+        try
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+        catch (JsonException)
+        {
+            // Pokud jsou data poškozená, je bezpečnější vrátit default (nebo klíč promazat)
+            return default;
+        }
+    }
+
+    public ValueTask RemoveAsync(string key)
+        => js.InvokeVoidAsync("localStorage.removeItem", key);
+
+    public ValueTask ClearAsync()
+        => js.InvokeVoidAsync("localStorage.clear");
+}
