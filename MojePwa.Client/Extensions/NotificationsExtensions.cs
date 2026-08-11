ab544de182e1bb7a46b074b60d88704ebada8ecb
Martin Sebera <xmartin.sebera@gmail.com>
2026-08-11 20:43:52 +0200

    Notif extensions

---
+++ MojePwa.Client/Extensions/NotificationsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Radzen;
+
+namespace MojePwa.Client.Extensions;
+
+/// <summary>
+/// Slouží ke zkrácení zápisů
+/// </summary>
+public static class NotificationsExtensions
+{
+    public static void Error(this NotificationService n, string summary, string detail = "", Action<NotificationMessage>? onClick = null)
+        => n.Notify(NotificationSeverity.Error, summary, detail, click: onClick);
+
+    public static void Warn(this NotificationService n, string summary, string detail = "", Action<NotificationMessage>? onClick = null)
+        => n.Notify(NotificationSeverity.Warning, summary, detail, click: onClick);
+
+    public static void Info(this NotificationService n, string summary, string detail = "", Action<NotificationMessage>? onClick = null)
+        => n.Notify(NotificationSeverity.Info, summary, detail, click: onClick);
+
+    public static void Success(this NotificationService n, string summary, string detail = "", Action<NotificationMessage>? onClick = null)
+        => n.Notify(NotificationSeverity.Success, summary, detail, click: onClick);
+}
